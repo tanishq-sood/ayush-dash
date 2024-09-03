05e9c98086cb4cbb6de1fb8052e8a37fec83dcb6
Akshat Jain <akshat00jain@gmail.com>
2024-09-03 23:11:46 +0530

    chore: Add checkUserCredentials function for user authentication

---
+++ src/lib/utils/DB_Actions.ts
@@ -0,0 +1,13 @@
+import DBclient from "@/lib/db";
+import { User } from "next-auth";
+
+export async function checkUserCredentials(email: string, password: string): Promise<User | null> {
+    const db = DBclient.db("ayush");
+    const collection = db.collection("users");
+
+    const user = await collection.findOne({ email });
+    if (!user) return null;
+
+    return user as User;
+
+}

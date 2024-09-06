7f1344da25ba79e5e6ecb3e2ec78b75e8c6c50ad
Akshat Jain <akshat00jain@gmail.com>
2024-09-06 23:35:58 +0530

    Commit

--- src/app/dashboard/tracking/page.tsx
+++ src/app/dashboard/tracking/page.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import type { Metadata } from 'next'
 
 import Tracking from "@/components/Tracking/Tracking"
+
+export const metadata: Metadata = {
+  title: 'Application Tracking',
+  description: '...',
+}
 
 const TrackingPage = () => {
   return (
